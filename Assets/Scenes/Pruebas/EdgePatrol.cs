4ef1d534bb4732830590a90f03b31d7e1f7deefe
Elena UPC <elena.fernandez.leon@estudiantat.upc.edu>
2024-11-27 10:06:49 +0100

    patroling pepse getbready

---
+++ Assets/Scenes/Pruebas/EdgePatrol.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random; //IMPORTANT, coger el del Engine
+
+public class EdgePatrol : MonoBehaviour
+{
+    public Transform DetectionPoint; //de aquei solo tengo uno enfrente. podria tener más (alrededor del personaje)
+    public float speed = 5;
+    public LayerMask WhatIsGround;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EdgeDetected())
+        {
+            Turn();
+        }
+        Move();
+    }
+
+    private bool EdgeDetected()
+    {
+        return Physics.Raycast(DetectionPoint.position, Vector3.down, 1.15f, WhatIsGround);      //cojo la posicion del detection point, luego su direccion y luego la distancia a del suelo, para evitar que no caiga y finalmente que lo que vea sea el suelo.
+    }
+
+    private void Turn()
+    {
+        transform.Rotate(new Vector3(0, Random.Range(90, 270), 0)); //solo queremos rotacion sobre eje Y
+    }
+
+    private void Move()
+    {
+        transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/Scenes/Pruebas/WayPointPatrol.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 2;
+    public Transform[] WayPoints;
+    private int currentWaypointIndex;
+    public bool Move2D;
+    public float MinDistance = 0.01f;
+    Transform CurrentWaypoint => WayPoints[currentWaypointIndex];
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (CloseToWaypoint())
+        {
+            ChangeWaypoint();
+        }
+        Move();
+    }
+
+    private bool CloseToWaypoint()
+    {
+        return Vector3.Distance(transform.position, CurrentWaypoint.position) < MinDistance;
+    }
+
+    private void ChangeWaypoint()
+    {
+        //podemos hace un if para que no sume más Waypoints de los que hay o podemos hacer un módulo.
+        currentWaypointIndex++;
+        currentWaypointIndex = currentWaypointIndex % WayPoints.Length;
+    }
+    private void Move()
+    {
+        Vector3 target = CurrentWaypoint.position;
+        target.y = transform.position.y;
+        transform.LookAt(CurrentWaypoint);
+        transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
+    }
+}
