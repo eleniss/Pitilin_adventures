15772f77957d0348a75521bea37f5896685232ef
Fuetito <marina.martin.prieto@estudiantat.upc.edu>
2024-11-27 17:36:04 +0100

    interaccion boton

--- Assets/Scenes/PuertasInicio/PlayerInteract.cs
+++ Assets/Scenes/PuertasInicio/PlayerInteract.cs
@@ -10,16 +10,19 @@ public class PlayerInteract : MonoBehaviour //script detecta elementos interactu
     private float distance = 3f;
     [SerializeField]
     private LayerMask mask;
+    private PlayerUI playerUI;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<PlayerLook>().cam;
+        playerUI = GetComponent<PlayerUI>();
     }
 
     // Update is called once per frame
     void Update() 
     {//el Ray detecta colisiones y almacena info de la colisión. Es un rayo que va recto, por eso el transform y forward: estamos creando el rayo que detecta las colisiones (infinito si se deja tal cual)
+        playerUI.UpdateText(string.Empty); //cuando no apunte a lo que es interactuable, no hay texto
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * distance);
 
@@ -28,6 +31,7 @@ public class PlayerInteract : MonoBehaviour //script detecta elementos interactu
         { //dentro verificamos si tenemos delante un objeto interactivo (layer de interactable)
             if(hitInfo.collider.GetComponent<Interactable>() != null)
             {
+                //playerUI.UpdateText
                 Debug.Log(hitInfo.collider.GetComponent<Interactable>().mensaje); //cuando tengamos delante un objeto interactivo aparecerá el mensaje "E para abrir" o lo que sea
             }
 
